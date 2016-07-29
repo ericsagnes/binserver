e17cbaeb67523f2f6a093bce4519a70996b04454
Eric Sagnes <eric.sagnes@gmail.com>
2016-07-29 23:55:54 +0900

    add channel

--- release.nix
+++ release.nix
@@ -5,13 +5,43 @@
 
 let
 
+  mkChannel = { src, constituents ? [], name }:
+    pkgs.stdenv.mkDerivation {
+      inherit name src constituents;
+      preferLocalBuild = true;
+      _hydraAggregate = true;
+
+      phases = [ "unpackPhase" "installPhase" ];
+
+      installPhase = ''
+        mkdir -p "$out/tarballs" "$out/nix-support"
+
+        tar cJf "$out/tarballs/nixexprs.tar.xz" \
+          --owner=0 --group=0 --mtime="1970-01-01 00:00:00 UTC" \
+          --transform='s!^\.!${name}!' .
+        
+        echo "channel - $out/tarballs/nixexprs.tar.xz" \
+          > "$out/nix-support/hydra-build-products"
+
+        echo $constituents > "$out/nix-support/hydra-aggregate-constituents"
+        for i in $constituents; do
+          if [ -e "$i/nix-support/failed" ]; then
+            touch "$out/nix-support/failed"
+          fi
+        done
+      '';
+      meta = {
+       isHydraChannel = true;
+      };
+    };
+
   # vanilla nixpkgs
   pkgs = import nixpkgs {};
 
   # build.nixをインポートするヘルパー
   buildFun = import ./build.nix;
 
-  jobs = {
+  jobs = rec {
     # 各システムのパッケージを生成する
     # -> { "i686-linux" = ...; "x86_64-linux" = ...; };
     build = pkgs.lib.genAttrs systems (system:
@@ -20,6 +50,12 @@ let
         pkgs = import nixpkgs { inherit system; };
       }
     );
+
+    channel = mkChannel {
+      name = "binserver-channel";
+      constituents = pkgs.lib.attrValues build;
+      src = ./.; 
+    };
   };
 
 in
